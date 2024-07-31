aba48733ec280b00fad429f2f80e074c0c83db25
dev-prakhar <prakhar02ver@gmail.com>
2024-07-31 12:38:44 +0530

    added data validaters

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,23 @@
 const express = require("express");
-const User = require('../Models/User')
-// const { default: mongoose } = require("mongoose");
+const User = require('../Models/User'); // Assuming you have a User model in the specified path
 const router = express.Router();
-// const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
-//Create a User using: POST "/api/auth"
-router.post("/", (req, res) => {
-console.log(req.body);
-const user = User(req.body);
-user.save();
-res.send(req.body);
+// Create a User using: POST "/api/auth"
+router.post("/", [
+  body('name').isLength({ min: 3 }).withMessage('Name must be at least 3 characters long'),
+  body('email').isEmail().withMessage('Invalid email address'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  User.create({
+    name: req.body.name,
+    password: req.body.password,
+    email: req.body.email,
+  }).then(user => res.json(user));
 });
+
 module.exports = router;
--- routes/notes.js
+++ routes/notes.js
@@ -2,12 +2,21 @@ const express = require("express");
 const Notes = require('../Models/Notes')
 const { default: mongoose } = require("mongoose");
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const notes = Notes(req.body);
-  notes.save();
-  res.send(req.body);
 
+router.post("/", [
+  body('title').isLength({ min:5 }).withMessage('title must be at least 5 characters long'),
+  body('description').isLength({max:200}).withMessage('gh'),
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  Notes.create({
+    title: req.body.title,
+    description: req.body.description,
+    
+  }).then(notes => res.json(notes));
 });
 module.exports = router;
