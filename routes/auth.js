44b7b614f82c49fc66b09e87c567918fb25cfa30
dev-prakhar <prakhar02ver@gmail.com>
2024-08-01 09:58:20 +0530

    managed requests using thunderclient

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,45 @@
 const express = require("express");
-const User = require('../Models/User'); // Assuming you have a User model in the specified path
+const User = require("../Models/User"); // Assuming you have a User model in the specified path
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
-// Create a User using: POST "/api/auth"
-router.post("/", [
-  body('name').isLength({ min: 3 }).withMessage('Name must be at least 3 characters long'),
-  body('email').isEmail().withMessage('Invalid email address'),
-  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
-], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+// Create a User using: POST "/api/auth/createuser"
+router.post(
+  "/createuser",
+  [
+    body("name")
+      .isLength({ min: 3 })
+      .withMessage("Name must be at least 3 characters long"),
+    body("email").isEmail().withMessage("Invalid email address"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //Chech whether user with this email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry, a user with this email already exists" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      });
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured");
+    }
   }
-  User.create({
-    name: req.body.name,
-    password: req.body.password,
-    email: req.body.email,
-  }).then(user => res.json(user));
-});
+);
 
 module.exports = router;
