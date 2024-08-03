145bb551874dc6825e3353a7dc54a8388db7e97f
dev-prakhar <prakhar02ver@gmail.com>
2024-08-03 19:22:37 +0530

    added CORS

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const mongoURI =
-  "mongodb://localhost:27017/?directConnection=true&readPreference=primary";
+  "mongodb://localhost:27017/";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI);
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const connectToMongo = require("./db");
+var cors = require('cors')
 connectToMongo();
 
 const express = require("express");
@@ -6,6 +7,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+app.use(cors())
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
