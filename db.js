8479cdb358b7ec80ab6ef1e80ba26ad5e08cf950
dev-prakhar <prakhar02ver@gmail.com>
2024-08-09 12:21:50 +0530

    changed database uri

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const mongoURI =
-  "mongodb://localhost:27017/";
+  "mongodb://localhost:27017/iNote?directConnection=true&readPreference=primary";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI);
